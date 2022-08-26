b38db93be148c65e6b4b730aba3ccc3d3edb82d1
Flu3nt <gnedyalkov94@gmail.com>
2022-08-26 18:20:04 +0300

    Setup the middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
 const port = process.env.PORT || 3007;
+
+const app = express();
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+  })
+);
 
 app.listen(port, () => {
   console.log(`I am listening my brother ${port}`);
